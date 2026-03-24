81c1b070d0b63ef41b07cc5c6e0ea19991df53ab
Tomate0613 <69756782+Tomate0613@users.noreply.github.com>
2026-03-24 23:14:16 +0100

    add flake with dev shell

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+    }:
+    let
+      forAllSystems =
+        function:
+        nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (
+          system: function nixpkgs.legacyPackages.${system}
+        );
+    in
+    {
+      devShells = forAllSystems (pkgs: {
+
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            packwiz
+            jdk21
+            (python3.withPackages (python-pkgs: [
+              python-pkgs.tomli-w
+              python-pkgs.requests
+            ]))
+          ];
+        };
+      });
+    };
+}
